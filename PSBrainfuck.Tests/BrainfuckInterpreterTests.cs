70c7604e372daccaaf217bd9edbadefc1cbc704d
psacuto <patrice.sacuto@gmail.com>
2015-03-01 14:02:23 +0100

    Remplacement de MSUnit par xUnit

--- PSBrainfuck.Tests/BrainfuckInterpreterTests.cs
+++ PSBrainfuck.Tests/BrainfuckInterpreterTests.cs
@@ -1,36 +1,35 @@
 ﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit;
 using System.IO;
 using System.Text;
 
 namespace PSBrainfuck.Tests
 {
-    [TestClass]
     public class BrainfuckInterpreterTests
     {
 
-        [TestMethod]
+        [Fact]
         public void TestEmptyString()
         {
             BrainfuckInterpreter bi = new BrainfuckInterpreter();
             string result = bi.Interpret("");
-            Assert.AreEqual("", result);
+            Assert.Equal("", result);
         }
 
-        [TestMethod]
+        [Fact]
         public void TestHelloWorld()
         {
             BrainfuckInterpreter bi = new BrainfuckInterpreter();
             string result = bi.Interpret("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.");
-            Assert.AreEqual("Hello World!\n", result);
+            Assert.Equal("Hello World!\n", result);
         }
 
-        [TestMethod]
+        [Fact]
         public void TestHelloWorld2()
         {
             BrainfuckInterpreter bi = new BrainfuckInterpreter();
             string result = bi.Interpret(">++++++++[<+++++++++>-]<.>>+>+>++>[-]+<[>[->+<<++++>]<<]>.+++++++..+++.>>+++++++.<<<[[-]<[-]>]<+++++++++++++++.>>.+++.------.--------.>>+.>++++.");
-            Assert.AreEqual("Hello World!\n", result);
+            Assert.Equal("Hello World!\n", result);
         }
     }
 }
